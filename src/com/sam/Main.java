529919bbba02be9489f4d52d6d77f97c6da34766
arutyusa <arutyusa@umich.edu>
2018-10-23 08:46:29 -0400

    committing from sourcetree, branch.

--- src/com/sam/Main.java
+++ src/com/sam/Main.java
@@ -2,7 +2,9 @@ package com.sam;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 	// write your code here
+        System.out.println("change");
     }
 }
