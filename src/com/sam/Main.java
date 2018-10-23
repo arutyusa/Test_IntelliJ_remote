7d6e2171403807879d5f40ce97c0de5e067d8193
arutyusa <arutyusa@umich.edu>
2018-10-23 08:56:30 -0400

    Revert "committing from sourcetree, branch."
    This reverts commit 529919bbba02be9489f4d52d6d77f97c6da34766.

--- src/com/sam/Main.java
+++ src/com/sam/Main.java
@@ -2,9 +2,7 @@ package com.sam;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 	// write your code here
-        System.out.println("change");
     }
 }
